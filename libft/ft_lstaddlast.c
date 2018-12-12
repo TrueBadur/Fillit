75fb4052cf70a3bb59afdaaee2011134b14c2f6b
Merlon crakehall Bartoletti <mbartole@e3r2p12.21-school.ru>
2018-12-12 14:02:53 +0300

    Fix ft_lstaddlast: work with null list

--- libft/ft_lstaddlast.c
+++ libft/ft_lstaddlast.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 14:32:49 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/27 19:00:21 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/12/12 11:50:42 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ void	ft_lstaddlast(t_list **alst, t_list *new)
 {
 	t_list *tmp;
 
-	if (!alst || !*alst || !new)
+	if (!alst || !new)
 		return ;
+	if (!*alst)
+	{
+		*alst = new;
+		return;
+	}
 	tmp = *alst;
 	while (tmp->next)
 		tmp = tmp->next;
