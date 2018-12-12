76c5a781a67eef954378f4654456de9ea07f83ec
Merlon crakehall Bartoletti <mbartole@e3r2p12.21-school.ru>
2018-12-12 19:03:16 +0300

    improve_fogs: start function check_valid

--- improve_figs.c
+++ improve_figs.c
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 16:26:49 by mbartole          #+#    #+#             */
-/*   Updated: 2018/12/12 17:02:39 by mbartole         ###   ########.fr       */
+/*   Updated: 2018/12/12 19:00:56 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ static int		max4(int a, int b, int c, int d)
 		return (a);
 	if (b > c && b > d)
 		return (b);
-	if (c > d)
-		return (c);
-	return (d);
+	return (c > d ? c : d);
 }
 
 static int		min4(int a, int b, int c, int d)
@@ -29,16 +27,7 @@ static int		min4(int a, int b, int c, int d)
 		return (a);
 	if (b < c && b < d)
 		return (b);
-	if (c < d)
-		return (c);
-	return (d);
-}
-
-static char		this_fucking_tetrimino(t_tet *t)
-{
-	return (t->data[0] == 0 && t->data[1] == 1 && t->data[2] == 1 &&
-			t->data[3] == 0 && t->data[4] == 1 && t->data[5] == 1 &&
-			t->data[6] == 2 && t->data[7] == 1);
+	return (c < d ? c : d);
 }
 
 static void		move(t_tet *t, int col, int shift)
@@ -48,6 +37,14 @@ static void		move(t_tet *t, int col, int shift)
 	i = -1;
 	while (++i < 4)
 		t->data[2 * i + col] = t->data[2 * i + col] - shift;
+}
+
+static void		check_valid(t_tet *t)
+{
+	if (t->h * t->w > 6)
+		exit(put_er(0));
+	if (t->h * t->w < 6)
+		return ;
 }
 
 t_tet			*improve_tet(t_tet *one)
@@ -60,9 +57,10 @@ t_tet			*improve_tet(t_tet *one)
 		move(one, 1, min);
 	one->w = max4(COLS(one)) + 1;
 	one->h = max4(ROWS(one)) + 1;
+	check_valid(one);
 	if (one->data[0] + one->data[1] == 0)
 		;
-	else if (this_fucking_tetrimino(one))
+	else if (FUCKING_TET(one))
 	{
 		move(one, 1, one->w - 1);
 		one->w = -one->w;
