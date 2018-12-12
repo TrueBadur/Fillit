83e193462bcf9c40289790651e786b3103e9c411
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-12-12 20:09:55 +0300

    Continue working on workitbaby. It's close but not yet

--- workitbaby.c
+++ workitbaby.c
@@ -6,73 +6,127 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 17:22:33 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/12/11 23:05:31 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/12/12 20:07:46 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int	place_fig(t_board *board, t_tet fig, int block)
+/*
+ * tries to place given figure in given pos
+ * or deletes given figure from given pos
+ * depending on f
+ */
+
+static int	place_fig(t_board *b, t_tet *fig, unsigned char posf, char f)
 {
 	char	*i;
+	char	j;
+	char	ro;
+	char	co;
+	char	c;
 
-	if (block >= 8)
-		return (1);
-	i = ((char *)(fig->data)) + block;
-	if (((char **)board->data)[board->row + *i][board->col + *(i + 1)] != '.')
-		return (0);
-	if (place_fig(board, fig, block + 2))
+	i = ((char *)(fig->data)) + 8;
+	ro = posf / b->w;
+	co = posf % b->w;
+	c = (f == 1) ? '.' : fig->l;
+	while (--f > 0)
 	{
-		((char **)board->data)[board->row + *i][board->col + *(i + 1)] = fig->l;
-		return (1);
+		j = 10;
+		while (j -= 2)
+		{
+			if (f == 2)
+			{
+				if (((char **)b->data)[ro + *(i - j)][co + *(i - j + 1)] != '.')
+					return (0);
+			}
+			else
+				((char **)b->data)[ro + *(i - j)][co + *(i - j + 1)] = c;
+		}
+	}
+	return (1);
+}
+
+/* 
+ * cicle through the board trying to put figure in each space
+ * if figures width or height is in the boards boundaries
+ */
+
+static short	find_place_fig(t_board *board, t_tet *cur, unsigned short posf)
+{
+	char row;
+	char col;
+
+	row = posf / board->w + cur->h;
+	col = posf % board->w + cur->w;
+	while (row > 0 && row <= board->w)
+	{
+		if (col > 0 && col <= board->w)
+			if (place_fig(board, cur, posf, 2))
+				return (posf);
+		posf++;
+		row = posf / board->w + cur->h;
+		col = posf % board->w + cur->w;
 	}
 	return (0);
 }
 
-static void	unplace_fig(t_board b, t_tet f)
+static unsigned short find_free(t_board *b, short posf, short posi, char di)
 {
-	char	*i;
-	int		j;
+	char	ro;
+	char	co;
 
-	i = ((char *)(f->data)) + 8;
-	j = 8;
-	while (j)
+	//check posf pos 
+	while (posf < board->w * board->w)
 	{
-		((char **)b->data)[b->row + *(i - j)][b->col + *(i - j + 1)] = '.';
-		j -= 2;
+		if (((char **)b->data)[posf / b->w][posf % b->w] != '.')
+		{
+			if(((char **)b->data)[posf / b->w + 1][posf % b->w] == '.'
+				|| ((char **)b->data)[posf / b->w][posf % b->w + 1] == '.')
+				return (posf);
+			if ((posf > b->w && posf % b->w 
+		}
 	}
+	//if it's not filled
+	// if at least one of directions is free 
+	//  return this pos
+	//move on
+	return (0);
 }
 
-void		workitbaby(t_list *figs, t_board *board, int col, int row)
+void			workitbaby(t_list *figs, t_board *board, unsigned short posf)
 {
-	t_list	*cur;
-	t_list	*prev;
+	t_list			*cur;
+	t_list			*prev;
+	unsigned short	posi;
 
-	cur = figs;
-	prev = NULL;
 	if (!figs)
 	{
-		print_board(board);
-		exit();
+		//	print_board(board);
+		exit(0);
 	}
+	cur = figs;
+	prev = NULL;
 	while (cur)
 	{
-		while (row + cur->h <= board->h)
+		//while there are figures
+		//try to place current fig somewhere on board
+		//if success 
+		// find first free space and store it
+		// store point of insertion of curr fig
+		// call recursive with trimed list of figs and first free point
+		// if recursion returned
+		//  remove fig from insertion point
+		//  back find last free space
+		if ((posi = find_place_fig(board, cur->content, posf)))
 		{
-			while (col + cur->w + j <= board->w)
-			{
-				if (place_fig(board, cur, 0))
-				{
-					if (!prev)
-						figs = cur->next;
-					else
-						prev->next = cur->next;
-					workitbaby(figs, board, col + 1, row);
-					unplace_fig(board, cur);
-				}
-				col++;
-			}
-			row++;
+			posf = find_free(board, posf, posi, 1);
+			if (!prev)
+				figs = cur->next;
+			else
+				prev->next = cur->next;
+			workitbaby(figs, board, posf);
+			place_fig(board, cur->content, posi, 1);
 		}
 		prev = cur;
 		cur = cur->next;
