9e071d81ace4a25035d4f44c6ee0f8896c27ec42
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-12-13 23:00:54 +0300

    Printfs for debuging in main and workitbaby. Fillit works with easy cases but works very long with some hard ones

--- workitbaby.c
+++ workitbaby.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 17:22:33 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/12/13 18:04:45 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/12/13 22:16:03 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,6 @@ static int	place_fig(t_board *b, t_tet *fig, unsigned char posf, char f)
 		{
 			if (f == 1)
 			{
-				printf("Before check for . posf = %d\n", posf);
-				printf("Checking col %d\n", co + *(i - j + 1));
 				if ((b->data)[ro + *(i - j)][co + *(i - j + 1)] != '.')
 					return (0);
 			}
@@ -64,10 +62,9 @@ static short	find_place_fig(t_board *board, t_tet *cur, unsigned short posf)
 	col = posf % board->w + cur->w;
 	while (row <= board->w && posf < board->w * board->w)
 	{
-		if (row >= 0 && col >= 0 && col <= board->w)
+		//printf("Checking row = %d, col = %d for fig %c\n", row, col, cur->l);
+		if (row >= -1 && col >= -1 && col <= board->w)
 		{
-			printf("Before place_fig in %d\n", posf);
-			print_board(board);
 			if (place_fig(board, cur, posf, 2))
 			{
 				return (posf);
@@ -114,20 +111,24 @@ int			workitbaby(t_list *figs, t_board *board, unsigned short posf)
 	t_list			*cur;
 	t_list			*prev;
 	short	posi;
+	short	posf_c;
 
 	if (!figs)
 	{
 		print_board(board);
 		return (1);
 	}
-	printf("------Start of workitbaby-----\n");
-	print_board(board);
 	cur = figs;
 	prev = NULL;
+	posf_c = posf;
 	while (cur)
 	{
-		ft_putendl("HELLO!");
-		printf("Trying to put %c\n", ((t_tet *)(cur->content))->l);
+		//printf("In while testing %c\n", ((t_tet*)(cur->content))->l);
+		if (((t_tet*)(cur->content))->l > 'Q')
+		{
+			print_board(board);
+			printf("\n");
+		}
 		if ((posi = find_place_fig(board, cur->content, posf)) != -1)
 		{
 			posf = find_free(board, posf);
@@ -135,15 +136,15 @@ int			workitbaby(t_list *figs, t_board *board, unsigned short posf)
 				figs = cur->next;
 			else
 				prev->next = cur->next;
-	print_board(board);
+		//	printf("posf = %d\n", posf);
 			if (workitbaby(figs, board, posf))
 				return (1);
-			printf("+++++++++++Returned from recursion++++++++\n");
 			place_fig(board, cur->content, posi, 1);
 			if (!prev)
 				figs = cur;
 			else
 				prev->next = cur;
+			posf = posf_c;
 		}
 		prev = cur;
 		cur = cur->next;
