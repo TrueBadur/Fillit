8f75921455d643415e09f8c18225302c2fb70768
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-12-15 14:53:24 +0300

    Fix main: NORM

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 14:32:09 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/12/14 01:18:18 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/12/15 14:44:41 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static int	get_board_size(int fc)
 	return (i);
 }
 
-int main (int ac, char **av)
+int			main (int ac, char **av)
 {
 	int		fd;
 	t_list	*figs;
@@ -31,28 +31,22 @@ int main (int ac, char **av)
 	int		c;
 
 	if (ac == 1 || ac > 2)
-	{
-//		pr_usage();				//TODO
-		return (1);
-	}
+		;
+//		return (pr_usage());				//TODO
 	if ((fd = open(av[1], O_RDONLY)) == -1)
-	{
-		ft_putendl("error");
-		return (2);
-	}
+		return (put_er(2));
 	figs = read_figs(fd, &c);
 	c = get_board_size(c * 4);
 	while (1)
 	{
-		if (!(board = ft_makeboard(c)))	//TODO
+		if (!(board = ft_makeboard(c++)))
 			return (3);
 		if (workitbaby(figs, board, 0))
 		{
 			free_board(&board);
 			ft_lstdel(&figs, NULL);
 			close(fd);
-			return (0);					//TODO
+			return (0);
 		}
-		c++;
 	}
 }
